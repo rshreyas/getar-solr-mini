1182e6e8ff49df83aca3d597cc2d05471d026ff0
rshreyas <32344774+rshreyas@users.noreply.github.com>
2017-10-15 10:21:35 -0700

    Add files via upload

--- abc.py
+++ abc.py
@@ -1,22 +1,23 @@
+import pandas as pd
+
 def create_lat_lon_field():
-	i_f = open('Dataset_z_46_5736178_tweets.csv', 'r')
-	o_f = open('Dataset_z_46_5736178_tweets_mod.csv', 'w')
+	i_f = open('Dataset_z_863_94783_tweets.csv', 'r')
+	#o_f = open('Dataset_z_46_5736178_tweets_mod.csv', 'w')
+	temp = []
+	df = pd.DataFrame([])
+	
 	for line in i_f:
 		cols = line.split(',')
 		cols = [x.strip() for x in cols]
 		col_a = cols[10]
 		col_b = cols[11]
-		new_line = ''
-		for i in cols:
-			if not new_line:
-				new_line = i
-			else:
-				new_line = new_line + '	' + i
 		if not (col_a or col_b):
 			col_a = '0.0'
 			col_b = '0.0'
-		new_line = new_line + '	' + col_a+','+col_b
-		o_f.write(new_line+'\n')
+		cols.append(col_a+','+col_b)
+		temp.append(cols)
+	df = df.append(temp)
+	df.to_csv(path_or_buf = 'Dataset_z_863_94783_tweets_mod.csv', sep = '\t', index = False, header = False)
 
 if __name__ == '__main__':
 	create_lat_lon_field()
